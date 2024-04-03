5f5c0cc9574b7257afa1dca33b5c95a4e9ed4d51
LeinerViloria <lvilora@siesa.com>
2024-04-02 21:33:53 -0500

    Ajustes

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	// "strings"
+	//"crypto/tls"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
+	// "github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
+	"net/smtp"
 )
 
 var UserEmail string
@@ -18,35 +21,35 @@ var SecretKey string
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	SecretKey := os.Getenv("SECRET_KEY")
+	// SecretKey := os.Getenv("SECRET_KEY")
 
 	r.Use(func(c *gin.Context) {
-		header := c.GetHeader("Authorization")
+		// header := c.GetHeader("Authorization")
 
-		tokenString := strings.Split(header, " ")[1]
+		// tokenString := strings.Split(header, " ")[1]
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(SecretKey), nil
-		})
+		// token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// 	return []byte(SecretKey), nil
+		// })
 
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
-			c.Abort()
-			return
-		}
+		// if err != nil {
+		// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		// 	c.Abort()
+		// 	return
+		// }
 
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			value := claims["user"].(string)
+		// if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// 	value := claims["user"].(string)
 
-			var data map[string]string
-			json.Unmarshal([]byte(value), &data)
+		// 	var data map[string]string
+		// 	json.Unmarshal([]byte(value), &data)
 
-			UserEmail = data["Email"]
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
-			c.Abort()
-			return
-		}
+		// 	UserEmail = data["Email"]
+		// } else {
+		// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		// 	c.Abort()
+		// 	return
+		// }
 
 		c.Next()
 	})
@@ -56,6 +59,28 @@ func setupRouter() *gin.Engine {
 	})
 
 	r.POST("/SendMessage", func(c *gin.Context) {
+
+		from := "t84098030@gmail.com"
+
+		// Configuración del servidor SMTP
+		smtpHost := "smtp.gmail.com"
+		smtpPort := "587"
+		auth := smtp.PlainAuth("", from, "OtraPruebaCecar123", smtpHost)
+
+		// Mensaje de correo electrónico
+		message := []byte("Subject: Prueba de correo electrónico\r\n" +
+			"\r\n" +
+			"Este es un mensaje de prueba enviado desde Go.\r\n")
+
+		to := []string{"pokop58224@evimzo.com"}
+
+		// Envío del correo electrónico
+		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+		if err != nil {
+			fmt.Printf("Error al enviar el correo electrónico: %v", err)
+			return
+		}
+
 		c.String(http.StatusOK, "Email sended")
 	})
 
